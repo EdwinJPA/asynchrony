2f2945a5d97eb991807e226554c1e3bb7bbbc370
J6-Babbage <edwinjpr11@gmail.com>
2022-09-13 16:28:27 -0500

    docs: update documentation about generators

--- src/generador/explicacion.js
+++ src/generador/explicacion.js
@@ -10,12 +10,15 @@ const g = gen();
 console.log(g.next().value);
 console.log(g.next().value);
 
+// Palabra reservada para el generador
 function* iterable(array) {
   for (let value of array) {
     yield value;
   }
 }
 
+// Se asigna a una variable el generador con su argumento 
 const it = iterable([1, 2, 3, 4, 5]);
+// Se ejecutan los yield con el método next
 console.log(it.next().value);
 console.log(it.next().value);
